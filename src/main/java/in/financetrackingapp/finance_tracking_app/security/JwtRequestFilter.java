be98c8b9a5d85e957cf1fbb90e9f0385f46f9dee
abhey <abheykumar7860@gmail.com>
2025-10-24 13:31:48 +0530

    Made changes

--- src/main/java/in/financetrackingapp/finance_tracking_app/security/JwtRequestFilter.java
+++ src/main/java/in/financetrackingapp/finance_tracking_app/security/JwtRequestFilter.java
@@ -1,5 +1,4 @@
 package in.financetrackingapp.finance_tracking_app.security;
-
 import in.financetrackingapp.finance_tracking_app.util.JwtUtil;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -48,58 +47,3 @@ public class JwtRequestFilter extends OncePerRequestFilter {
         filterChain.doFilter(request, response);
     }
 }
-
-
-
-
-
-
-
-//package in.financetrackingapp.finance_tracking_app.security;
-//import in.financetrackingapp.finance_tracking_app.service.AppUserDetailsService;
-//import in.financetrackingapp.finance_tracking_app.util.JwtUtil;
-//import jakarta.servlet.FilterChain;
-//import jakarta.servlet.ServletException;
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpServletResponse;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-//import org.springframework.security.core.context.SecurityContextHolder;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-//import org.springframework.stereotype.Component;
-//import org.springframework.web.filter.OncePerRequestFilter;
-//
-//import java.io.IOException;
-//
-//@Component
-//@RequiredArgsConstructor
-//public class JwtRequestFilter extends OncePerRequestFilter {
-//
-//    private final JwtUtil jwtUtil;
-//    private final AppUserDetailsService appUserDetailsService;
-//
-//    @Override
-//    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
-//         String authHeader = request.getHeader("Authorization");
-//         String email = null;
-//         String jwt = null;
-//
-//        if(authHeader != null && authHeader.startsWith("Bearer ")){
-//           jwt = authHeader.substring(7);
-//           email = jwtUtil.extractUsername(jwt);
-//        }
-//        if(email != null && SecurityContextHolder.getContext().getAuthentication() == null){
-//            UserDetails userDetails = this.appUserDetailsService.loadUserByUsername(email);
-//            if (jwtUtil.validateToken(jwt, userDetails.getUsername())){
-//                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
-//                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-//                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-//            }
-//        }
-//        chain.doFilter(request,response);
-//    }
-//}
-//
-//
-//
