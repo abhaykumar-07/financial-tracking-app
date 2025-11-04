b822099ebb5e2af7edd3789c5166ea0ad7fab82a
abhaykumar-07 <abhaykumar200214@gmail.com>
2025-11-05 00:28:40 +0530

    Update SecurityConfig.java

--- src/main/java/in/financetrackingapp/finance_tracking_app/config/SecurityConfig.java
+++ src/main/java/in/financetrackingapp/finance_tracking_app/config/SecurityConfig.java
@@ -48,7 +48,14 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(List.of("*"));
+        // configuration.setAllowedOriginPatterns(List.of("*"));
+
+        configuration.setAllowedOrigins(List.of(
+        "http://localhost:5173",
+        "https://financial-tracking-app-yust.onrender.com",
+        "https://your-frontend-domain.com"
+));
+
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization","Content-Type", "Accept"));
         configuration.setAllowCredentials(true);
